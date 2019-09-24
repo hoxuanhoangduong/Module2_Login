13d065cd8f7777cc4c2457e1b223ec1548d539f0
Ho Duong <hxhduong4394@gmail.com>
2019-09-24 15:01:15 +0700

    Add test servlet.

--- src/main/java/com/codegym/TestServlet.java
+++ src/main/java/com/codegym/TestServlet.java
@@ -1,7 +1,24 @@
 package com.codegym;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if ("admin".equals(username) && "admin".equals(password)) {
+            resp.getOutputStream().println("<h1>Ok</h1>");
+        } else {
+            resp.getOutputStream().println("Fall");
+        }
+
+    }
 
 }
